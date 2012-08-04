fa0aa2df355f14b246f781663b045b94bb2f2a64
Luke Sneeringer <luke@sneeringer.com>
2012-08-04 18:05:29 -0500

    Initial Commit

---
+++ Mega Man Password Generator/MMPGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MMPGAppDelegate.h
+//  Mega Man Password Generator
+//
+//  Created by Luke Sneeringer on 2012-08-04.
+//  Copyright (c) 2012 Luke Sneeringer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMPGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Mega Man Password Generator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Mega Man Password Generator
+//
+//  Created by Luke Sneeringer on 2012-08-04.
+//  Copyright (c) 2012 Luke Sneeringer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MMPGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MMPGAppDelegate class]));
+    }
+}
