28e4d63475f7e28c18a0250fefe65d363c39fed8
Luke Sneeringer <lukesneeringer@gmail.com>
2012-08-16 18:04:03 -0500

    Getting mm2_grid.png and mm3_grid.png out of the project file so I can re-add it.

--- Mega Man Password Generator/MM3PasswordGenerator.m
+++ Mega Man Password Generator/MM3PasswordGenerator.m
@@ -8,11 +8,21 @@
 
 #import "MM3PasswordGenerator.h"
 #import "Password.h"
+#import "Game.h"
+#import "Item.h"
 
 @implementation MM3PasswordGenerator
 
 - (Password*)generatePassword {
     NSMutableArray* units = [[NSMutableArray alloc] init];
+    
+    // set the dot for the number of energy tanks in the player's possession
+    Item* energyTanks = [game.items objectAtIndex:0];
+    NSArray* energyTanksArray = [NSArray arrayWithObjects:@"C5", @"E6", @"E4", @"B4", @"A5", @"C1", @"D2", @"C3", @"F2", @"A6", nil];
+    NSString* coordString = [energyTanksArray objectAtIndex:energyTanks.stock];
+    [units addObject:[[PasswordUnit alloc] initWithImageCode:@"red_dot" atRow:[coordString characterAtIndex:0] andColumn:(int)[coordString characterAtIndex:1]]];
+    
+    // return back the final password
     return [[Password alloc] initWithUnits:units];
 }
 
