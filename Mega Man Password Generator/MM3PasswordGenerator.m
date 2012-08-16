5e5f8fdb21ed46e4b9106feb39cd060e6fc058cf
Luke Sneeringer <lukesneeringer@gmail.com>
2012-08-16 18:24:39 -0500

    Adding password support for Top Man and Snake Man.

--- Mega Man Password Generator/MM3PasswordGenerator.m
+++ Mega Man Password Generator/MM3PasswordGenerator.m
@@ -9,6 +9,7 @@
 #import "MM3PasswordGenerator.h"
 #import "Password.h"
 #import "Game.h"
+#import "Boss.h"
 #import "Item.h"
 
 @implementation MM3PasswordGenerator
@@ -21,6 +22,19 @@
     NSArray* energyTanksArray = [NSArray arrayWithObjects:@"C5", @"E6", @"E4", @"B4", @"A5", @"C1", @"D2", @"C3", @"F2", @"A6", nil];
     NSString* coordString = [energyTanksArray objectAtIndex:energyTanks.stock];
     [units addObject:[[PasswordUnit alloc] initWithImageCode:@"red_dot" atRow:[coordString characterAtIndex:0] andColumn:(int)[coordString characterAtIndex:1] - 48]];
+    
+    // set the dot for Top Man and Snake Man
+    Boss* topMan = [game bossWithCode:@"top_man"];
+    Boss* snakeMan = [game bossWithCode:@"snake_man"];
+    if (topMan.defeated && snakeMan.defeated) {
+        [units addObject:[[PasswordUnit alloc] initWithImageCode:@"blue_dot" atRow:'A' andColumn:3]];
+    }
+    else if (topMan.defeated) {
+        [units addObject:[[PasswordUnit alloc] initWithImageCode:@"red_dot" atRow:'A' andColumn:3]];
+    }
+    else if (snakeMan.defeated) {
+        [units addObject:[[PasswordUnit alloc] initWithImageCode:@"red_dot" atRow:'F' andColumn:6]];
+    }
     
     // return back the final password
     return [[Password alloc] initWithUnits:units];
